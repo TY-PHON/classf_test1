bf9c553ce1f4c4ac2068ebcc526d58d07a3e5c58
pzc <1827933557@qq.com>
2023-03-08 21:13:56 +0800

    tt

--- model/test.py
+++ model/test.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
 
     # "cuda" when available
     #device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    device = torch.device('cpu')
+    device = torch.device('cuda')
 
     n_epochs = 4
     patience = 300 #if no improvement in 'patience' epochs, stop
